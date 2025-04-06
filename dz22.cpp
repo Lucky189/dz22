a707eec699d08e9bcbadd9cb69d0df008537ff3f
Бордюжа Марк <borduzamark@gmail.com>
2025-04-06 23:31:28 +0300

    Add files via upload

---
+++ dz22.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+class Parent
+{
+public:
+	Parent() {}
+
+	friend std::ostream& operator<<(std::ostream& out, const Parent& p)
+	{
+		return p.print(out);
+	}
+
+	virtual std::ostream& print(std::ostream& out) const
+	{
+		out << "Parent";
+		return out;
+	}
+};
+
+class Child : public Parent
+{
+public:
+	Child() {}
+
+	virtual std::ostream& print(std::ostream& out) const override
+	{
+		out << "Child";
+		return out;
+	}
+};
+
+int main()
+{
+	Parent p;
+	std::cout << p << '\n';
+
+	Child ch;
+	std::cout << ch << '\n'; 
+
+	Parent& pref = ch;
+	std::cout << pref << '\n';
+
+	return 0;
+}
